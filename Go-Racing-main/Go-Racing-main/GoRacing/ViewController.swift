68bee002fee685313dc71e12075b499588c96ab6
Kodi12354 <103703307+Kodi12354@users.noreply.github.com>
2022-05-16 10:11:17 +0900

    Add files via upload

--- Go-Racing-main/Go-Racing-main/GoRacing/ViewController.swift
+++ Go-Racing-main/Go-Racing-main/GoRacing/ViewController.swift
@@ -13,12 +13,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var userCar: UIImageView!
     @IBOutlet weak var resultLable: UILabel!
     @IBOutlet weak var lineFinish: UIImageView!
-    @IBOutlet weak var semaforLabel: UILabel!
     
-    @objc var intervalTimer: Timer!
     var stateSemafor: Int = 1
     var timerGame: Timer!
     var timerPc: Timer!
+    var semaforLabel: UILabel!
+    var intervalTimer: Timer!
     
     @objc func pcDrive() {
         if stateSemafor == 2 {
@@ -30,49 +30,46 @@ class ViewController: UIViewController {
             resultLable.textColor = .red
             timerPc.invalidate()
             timerGame.invalidate()
-        }
-    }
-        @objc func intrevalTimer() {
             
+            
+        }
+        @objc func intrevalTimer() {
             stateSemafor += 1
             if stateSemafor > 2 {
-                stateSemafor = 1
             }
         switch stateSemafor {
             case 1 :
                 semaforLabel.text = "STOP"
-                semaforLabel.textColor = .red
+            semaforLabel.textColor = .red
             case 2 :
-                semaforLabel.text = "DRIVE"
-                semaforLabel.textColor = .green
+                semaforLabel.text = "GO"
+            semaforLabel.textColor = .green
             default:
                 break
-    }
-}
-    @IBAction func driveGame(_ sender: UIButton) {
-        semaforLabel.isHidden = false
-        
-        timerGame =
-        Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(getter: intervalTimer), userInfo: nil, repeats: true)
-    
+        }
+        @IBAction func startGame(_ sender: UIButton) {
+            semaforLabel.isHidden = false
+            
+            timerGame = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(intrevalTimer), userInfo: nil, repeats: true)
             timerPc = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(pcDrive), userInfo: nil, repeats: true)
-}
-    @IBAction func driveGame(_ sender: UIButton) {
-        if stateSemafor == 2 {
-            userCar.center.x += 10
-        } else if stateSemafor == 1 {
-            userCar.center.x -= 10
+            }
+          
+        @IBAction  func driveGame(_ sender: UIButton) {
+            if stateSemafor == 2 {
+                userCar.center.x += 10
+            } else if stateSemafor == 1 {
+                userCar.center.x -= 10
+            }
+            if userCar.center.x > lineFinish.center.x {
+                resultLable.isHidden = false
+                resultLable.text = "YOU WIN"
+                resultLable.textColor = .green
+                timerPc.invalidate()
+            }
+            
+            }
         }
     }
-    
-    if userCar.center.x > lineFinish.center.x {
-        resultLable.isHidden = false
-        resultLable.text = "YOU WIN"
-        resultLable.textColor = .green
-        timerPc.invalidate()
-        timerGame.invalidate()
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -81,3 +78,4 @@ class ViewController: UIViewController {
 
 
 }
+
